0883254ec567d5f4e74c16830fee3cb1b4a9be68
Harsh <tripathiharsh16@gmail.com>
2022-01-12 16:34:37 +0530

    src files included for client side application

--- ToDoLists/contract/ToDoList.sol
+++ ToDoLists/contract/ToDoList.sol
@@ -1,6 +1,23 @@
 pragma solidity >=0.5.0 <=0.9.0;
 
 contract ToDoList {
-    uint public taskCount=0;
-    
+    uint public taskCount=0;  //state variable
+
+    struct Task {
+            uint id;
+            string content;
+            bool completed;
+    }
+
+    mapping( uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Hello Dapps");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
+
